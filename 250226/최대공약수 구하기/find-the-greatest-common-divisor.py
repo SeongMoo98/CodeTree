302c5c4f65d0acd3cdfc6ff0dfc823a2c970c354
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-26 18:44:14 +0900

    Title: 최대공약수 구하기, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 250226/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 
 def sol(n, m):
     max_num = 0
-    for i in range(1, min(n, m)):
+    for i in range(1, min(n, m)+1):
         if n % i == 0 and m % i == 0:
             max_num = i
     return max_num
