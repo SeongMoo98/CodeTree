cc878cf20eb745ffb00912af14d24cbea7204b89
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-26 19:22:43 +0900

    Title: 함수를 이용한 소수 판별, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 250226/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -2,14 +2,16 @@ a, b = map(int, input().split())
 
 # Please write your code here.
 def is_prime(num):
-    if num == 1 or num == 2:
+    if num == 1:
+        return False
+    if num == 2:
         return True
-        
+
     for i in range(2, num):
         if num % i == 0:
             return False
-
     return True
+
 def sol(A, B):
     res = 0
 
