c5187bdf09a15d6ad1032c9e44875d45817a5b21
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-27 00:18:05 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250227/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 250227/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -3,9 +3,12 @@ arr = list(map(int, input().split()))
 
 
 def sol(arr, N):
-    if len(arr) == 1 or len(arr) == 2:
-        return max(arr)
+    if len(arr) == 0:
+        return 0
+    
+    if len(arr) == 1:
+        return arr[0]
     N = N // 2
-    return max(sol(arr[:N], N), sol(arr[N:], N))
+    return max(sol(arr[:N], len(arr[:N])), sol(arr[N:], len(arr[N:])))
 
 print(sol(arr, N))
