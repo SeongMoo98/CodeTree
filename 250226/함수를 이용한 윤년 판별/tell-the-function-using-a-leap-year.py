c5997caf1b59c423c84653eba504abf09b3907bb
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-26 19:24:53 +0900

    Title: 함수를 이용한 윤년 판별, Time: 68ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250226/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,13 @@
+y = int(input())
+
+def sol(y)
+    if y % 4 == 0:
+        if y % 100 == 0 and y % 400 != 0:
+            return False
+        return True
+    return True
+
+if sol(y):
+    print("true")
+else:
+    print("false")
