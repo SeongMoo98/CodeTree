245d368c39429d9cca63f10ff43331611e293ce7
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-26 20:20:08 +0900

    Title: 함수를 이용한 온전수 판별, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 250226/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -5,7 +5,7 @@ a, b = map(int, input().split())
 count = 0
 for num in range(a, b+1):
     if num % 2 != 0:
-        if num % 10 != 5
+        if num % 10 != 5:
             if num % 3 == 0 and num % 9 != 0:
                 continue
             else:
