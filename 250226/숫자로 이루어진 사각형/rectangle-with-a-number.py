1a07e58f08eb0278872b0cefcda88c9584bac4a6
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-26 18:39:25 +0900

    Title: 숫자로 이루어진 사각형, Time: 64ms, Memory: 17MB, Status: Passed - CodeTree

--- 250226/숫자로 이루어진 사각형/rectangle-with-a-number.py
+++ 250226/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -4,7 +4,8 @@ num = 1
 for i in range(N):
     for j in range(N):
         print(num, end=' ')
-        if num < 10:
+        if num < 9:
             num += 1
         else:
             num = 1
+    print()
