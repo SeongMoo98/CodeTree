f262b406f0a4ba865edc5ad7e357f0b79d4a2a24
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-26 22:11:01 +0900

    Title: 두 정수에 대한 연산값, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 250226/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -2,10 +2,9 @@ a, b = map(int, input().split())
 
 # Please write your code here.
 def sol(a, b):
-    mini = min(a, b)
-    maxi = max(a, b)
-
-    return maxi+25, mini*2
-
-maxi, mini = sol(a, b)
-print(mini, maxi)
+    if a > b:
+        return a+25, b*2
+    else :
+        return a*2, b+25
+a, b = sol(a, b)
+print(a, b)
