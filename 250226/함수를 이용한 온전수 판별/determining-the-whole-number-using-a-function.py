ee00ee5d45dbb341779193eca4ef8d83e5ef2c0d
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-26 20:17:08 +0900

    Title: 함수를 이용한 온전수 판별, Time: 72ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250226/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+
+count = 0
+for num in range(A, B+1):
+    if num % 2 != 0 or num % 5 != 5:
+        if num % 3 == 0 and num % 9 !=0:
+            continue
+        else:
+            count += 1
+print(count)
