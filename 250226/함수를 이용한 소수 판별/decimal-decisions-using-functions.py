f0f99e001c7713ef66edc1aa266cfe920d4bb2ed
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-26 19:07:20 +0900

    Title: 함수를 이용한 소수 판별, Time: 72ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250226/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,18 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+
+    return True
+def sol(A, B):
+    res = 0
+
+    for num in range(A, B+1):
+        if is_prime(num):
+            res += 1
+
+    return res
+print(sol(A, B))
