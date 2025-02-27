091a34a322f67299d81106afbcaedcc86c7f75b9
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-27 15:42:27 +0900

    Title: 행복한 수열의 개수, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250227/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+# 각 행, 열에서 연속해서 M개 이상의 동일한 원소
+res = 0
+for i in range(n):
+    prev = grid[i][0]
+    count = 1
+    for j in range(1, n):
+        if count >= m:
+                res += 1
+                break
+
+        if prev == grid[i][j]:
+            count += 1
+        else:
+            prev = grid[i][j]
+            count = 1
+
+
+for j in range(n):
+    prev = grid[0][j]
+    count = 1
+    for i in range(1, n):
+        if count >= m:
+                res += 1
+                break
+        if prev == grid[i][j]:
+            count += 1
+
+        else:
+            prev = grid[i][j]
+            count = 1
+
+print(res)
