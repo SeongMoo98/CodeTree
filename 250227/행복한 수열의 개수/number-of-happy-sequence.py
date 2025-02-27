9c15068bf849d798d978ce1c9ab4a333a4ea66fc
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-02-27 15:51:10 +0900

    Title: 행복한 수열의 개수, Time: 64ms, Memory: 17MB, Status: Passed - CodeTree

--- 250227/행복한 수열의 개수/number-of-happy-sequence.py
+++ 250227/행복한 수열의 개수/number-of-happy-sequence.py
@@ -5,40 +5,37 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 # 각 행, 열에서 연속해서 M개 이상의 동일한 원소
 res = 0
-for i in range(n):
-    prev = grid[i][0]
-    count = 1
-    for j in range(1, n):     
-        if count >= m:
-            res += 1
-            break
-            
-        if prev == grid[i][j]:
-            count += 1
-        else:
-            prev = grid[i][j]
-            count = 1
+if n == 1:
+    res = 2
+else:
 
-        if count >= m:
-            res += 1
-            break
+    for i in range(n):
+        prev = grid[i][0]
+        count = 1
+        for j in range(1, n):     
+
+            if prev == grid[i][j]:
+                count += 1
+            else:
+                prev = grid[i][j]
+                count = 1
+
+            if count >= m:
+                res += 1
+                break
 
 
-for j in range(n):
-    prev = grid[0][j]
-    count = 1
-    for i in range(1, n):
-        if count >= m:
-            res += 1
-            break
-
-        if prev == grid[i][j]:
-            count += 1
-        else:
-            prev = grid[i][j]
-            count = 1
-        if count >= m:
-            res += 1
-            break
+    for j in range(n):
+        prev = grid[0][j]
+        count = 1
+        for i in range(1, n):
+            if prev == grid[i][j]:
+                count += 1
+            else:
+                prev = grid[i][j]
+                count = 1
+            if count >= m:
+                res += 1
+                break
 
 print(res)
