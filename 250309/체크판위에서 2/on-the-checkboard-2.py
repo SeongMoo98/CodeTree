8ea3a66c7ca76003d48dc0c430c8ce85cda5cff4
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-09 16:52:42 +0900

    Title: 체크판위에서 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250309/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,23 @@
+R, C = map(int, input().split())
+grid = [list(input().split()) for _ in range(R)]
+
+# Please write your code here.
+
+# 점프 : 현재 색 != 점프한 색
+# 적어도 한칸 이상 오른쪽 and 한칸 이상 아래쪽
+# 시작, 도착 지점을 제외하고 점프하며 도달한 위치가 정확히 2곳(즉, 점프를 3번)
+# 시작 -> 점프 1 -> 점프 2 -> 도착
+# (0, 0) -> (i, j) -> (l, m) -> (R-1, C-1)
+
+start = grid[0][0]
+count = 0
+for i in range(R):
+    for j in range(C):
+        if start != grid[i][j]:
+            curr = grid[i][j]
+            for l in range(i+1, R-1):
+                for m in range(j+1, C-1):
+                    if curr != grid[l][m]:
+                        count += 1 
+print(count)
+
