1de9fc41ff1f846dc7c8a8f8592477b2815912c6
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-09 16:26:50 +0900

    Title: 최고의 13위치, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250309/최고의 13위치/best-place-of-13.py
+++ 250309/최고의 13위치/best-place-of-13.py
@@ -2,10 +2,11 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-
+count = 0 
 for i in range(n):
-    count = grid[i][0:3].count(1)
+    count = max(count, grid[i][0:3].count(1))
+    print(count)
     for j in range(n-3):
-        count = max(count, grid[i][j:j+3])
+        count = max(count, grid[i][j:j+3].count(1))
 
 print(count)
