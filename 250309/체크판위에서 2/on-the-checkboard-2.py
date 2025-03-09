d93585f985c31ef58bba61343c2f45990d050dd9
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-09 17:28:49 +0900

    Title: 체크판위에서 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/체크판위에서 2/on-the-checkboard-2.py
+++ 250309/체크판위에서 2/on-the-checkboard-2.py
@@ -9,15 +9,14 @@ grid = [list(input().split()) for _ in range(R)]
 # 시작 -> 점프 1 -> 점프 2 -> 도착
 # (0, 0) -> (i, j) -> (l, m) -> (R-1, C-1)
 
-start = grid[0][0]
+start, end = grid[0][0], grid[R-1][C-1]
 count = 0
-for i in range(1, R-2):
-    for j in range(1, C-2):
+for i in range(1, R):
+    for j in range(1, C):
         if start != grid[i][j]:
             curr = grid[i][j]
             for l in range(i+1, R-1):
                 for m in range(j+1, C-1):
-                    if curr != grid[l][m]:
+                    if curr != grid[l][m] and grid[l][m] != end:
                         count += 1 
 print(count)
-
