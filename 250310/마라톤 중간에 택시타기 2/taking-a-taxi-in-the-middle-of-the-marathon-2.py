9ef254167a249f9344d2d301cc3b4c652528107a
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-10 19:55:57 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250310/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,27 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+# Please write your code here.
+# 1번 체크포인트 -> N번 체크포인트
+# 1, N번 체크포인트 제외 체크포인트 건너뛰기
+# 최소 거리(맨하탄)
+
+min_dist = float('inf')
+
+for skip_idx in range(1, n-1):
+    ci, cj = x[0], y[0]
+    dist = 0    
+    for j in range(1, n):
+        if j == skip_idx:
+            ci, cj = x[skip_idx-1], y[skip_idx-1]
+            continue
+        ni, nj = x[j], y[j]
+
+        dist += abs(ni - ci) + abs(nj - cj)
+        ci, cj = ni, nj
+        
+    min_dist = min(min_dist, dist)
+print(min_dist)
+
