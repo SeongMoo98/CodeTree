c8d57da95ef27ebc56ed37bdc49a28a8f8def192
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-11 13:54:54 +0900

    Title: 특정 구간의 원소 평균값, Time: 76ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250311/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -0,0 +1,17 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+
+# 특정 구간을 잡았을 때, 
+# 그 구간 안에 있는 원소들의 평균값이 그 구간의 원소 중 하나가 되게하는 서로 다른 가짓수
+
+# 1개, 2개 .. N개의 원소를 선택
+count = 0
+for inter in range(1, N+1):
+    for j in range(N):
+        # inter개 선택
+        temp = arr[j: j+inter]
+        if sum(temp) / inter in temp:
+            count += 1
+
+print(count)
