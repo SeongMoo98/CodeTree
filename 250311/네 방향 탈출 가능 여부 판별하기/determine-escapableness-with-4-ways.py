03768b3535bd0dd1020a717b887b6df2e38469bf
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-11 19:28:41 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 3749ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 250311/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,26 @@
+from collections import deque
+# N x M 좌측 상단에서 출발 -> 우측 하단 도착
+# 상하좌우
+# 뱀이 있는 칸 이동 X, 뱀에 물리지 않고 도착
+# 뱀에 물리지 않고 탈출 가능한지 여부 확인
+
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+# 상, 하, 좌, 우
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+visited = [[0] * M for _ in range(N)]
+visited[0][0] = 1
+q = deque([(0, 0)])
+
+while q:
+    ci, cj = q.popleft()
+    for di, dj in d:
+        ni, nj = ci + di, cj + dj
+        if 0 <= ni < N and 0 <= nj < M and (ni, nj) not in visited and graph[ni][nj] == 1:
+            visited[ni][nj] = 1
+            q.append((ni, nj))
+
+print(visited[N-1][M-1])
+
