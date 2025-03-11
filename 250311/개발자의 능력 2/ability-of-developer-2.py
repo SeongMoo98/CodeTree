f93337f3ce7d418097fe279d82650a2c3bbe2a91
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-11 16:06:13 +0900

    Title: 개발자의 능력 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250311/개발자의 능력 2/ability-of-developer-2.py
+++ 250311/개발자의 능력 2/ability-of-developer-2.py
@@ -2,17 +2,20 @@
 
 # 팀원들의 능력 총합이 가장 큰 팀과 가장 작은 팀의 차이를 최소화할 때
 # 최대팀과 최소 팀간의 차를 출력
-scores = map(int, input().split())
+n = 6
+scores = list(map(int, input().split()))
 total_sum = sum(scores)
+
 min_sub = float("inf")
-for i in range(3):
-    for j in range(i+1, 4):
-        team1 = scores[i] + scores[j]
-        for l in range(j+1, 5):
-            for m in range(l+1, 6):
-                team2 = scores[l] + scores[m]
-                team3 = total_sum - team1 - team2
-                max_team = max(team1, team2, team3)
-                min_team = min(team1, team2, team3)
-                min_sub = min(min_sub, max_team - min_team)
+for i in range(n):
+    for j in range(i+1, n):
+        # 얘는 왜 0부터 시작인가?
+        # (i, j)가 (0,2)일 때 (l, m)은 (1, 3)이 가능하기 때문이다
+        for l in range(n):
+            for m in range(l+1, n):
+                if l != i and l != j and m != i and m != j:
+                    team1 = scores[i] + scores[j]
+                    team2 = scores[l] + scores[m]
+                    team3 = total_sum - team1 - team2
+                    min_sub = min(min_sub, max(team1, team2, team3) - min(team1, team2, team3))
 print(min_sub)
