274ebd5f13d88ff8888a9d53d3a8501737c22c2e
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-11 13:39:00 +0900

    Title: G or H 3, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250311/G or H 3/G-or-H-3.py
+++ 250311/G or H 3/G-or-H-3.py
@@ -1,25 +1,25 @@
 n, k = map(int, input().split())
 x = []
 c = []
-# N명이 위치 x에서 G, H를 받음
-# G는 1, H는 2
-# 사진의 크기가 K
+
 for _ in range(n):
     pos, char = input().split()
     x.append(int(pos))
     c.append(char)
 
 # Please write your code here.
+# N명이 위치 x에서 G, H를 받음
+# G는 1, H는 2
+# 사진의 크기가 K
 
-max_x = max(x)
 # 위치는 1 ~ x
-positions = [0] * (max_x + 1) 
+positions = [0] * (max(x) + 1) 
 
 for pos, ch in zip(x, c):
     positions[pos] = ch
 
 max_score = 0
-for i in range(max_x-k+1):
+for i in range(1, max(x)-k+1):
     score = 0
     temp = positions[i:i+k+1]
     score += temp.count("G")
