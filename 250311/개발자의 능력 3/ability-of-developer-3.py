9b6da494a9b1f99aa03ba4d161394035fa468a01
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-11 15:47:54 +0900

    Title: 개발자의 능력 3, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250311/개발자의 능력 3/ability-of-developer-3.py
+++ 250311/개발자의 능력 3/ability-of-developer-3.py
@@ -6,18 +6,15 @@ scores = list(map(int, input().split()))
 total_sum = sum(scores)
 
 min_sub = float('inf')
-res = 0
 for i in range(4):
-    for j in range(1, 5):
-        for k in range(2, 6):
+    for j in range(i+1, 5):
+        for k in range(j+1, 6):
             team1 = scores[i] + scores[j] + scores[k]
             team2 = total_sum - team1
-            if abs(team1 - team2) < min_sub:
-                min_sub = abs(team1 - team2)
-                res = abs(team1 - team2)
+            min_sub = min(min_sub, abs(team1 - team2))
             
 
-print(res)    
+print(min_sub)    
 
 
         
