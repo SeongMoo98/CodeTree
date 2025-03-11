6a5a86de0082a38f9f0ec98b28f7f915de501c04
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-11 13:35:18 +0900

    Title: G or H 3, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250311/G or H 3/G-or-H-3.py
+++ 250311/G or H 3/G-or-H-3.py
@@ -15,19 +15,16 @@ max_x = max(x)
 # 위치는 1 ~ x
 positions = [0] * (max_x + 1) 
 
-for num, pos in zip(list(range(1, n+1)), x):
-    positions[pos] = num
+for pos, ch in zip(x, c):
+    positions[pos] = ch
 
 max_score = 0
 for i in range(max_x-k+1):
     score = 0
-    for j in range(i, i+k+1):
-        if positions[j] != 0:
-            if c[positions[j]-1] == "G":
-                score += 1
-            else:
-                score += 2
-
+    temp = positions[i:i+k+1]
+    score += temp.count("G")
+    score += 2 * temp.count("H")
+    
     max_score = max(max_score, score)
 
 print(max_score)
