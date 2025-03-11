333dfba8318cb1fccf1ba057a0263b63c9012b06
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-11 15:33:52 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250311/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -0,0 +1,17 @@
+# 1 ~ N을 중복해서 뽑아 총 3자리
+# 한 자리라도 주어지는 조합과 거리가 2 이내이면 열린다
+
+
+N = int(input())
+combination = tuple(map(int, input().split()))
+
+count = 0
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        for k in range(1, N+1):
+            if (abs(combination[0] - i) < 2) or \
+            (abs(combination[1] - j) < 2)\
+             or (abs(combination[2] - k) < 2):
+             count += 1
+print(count)
+
