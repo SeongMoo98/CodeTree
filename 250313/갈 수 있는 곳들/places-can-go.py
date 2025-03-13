07016b044a355b927d729845a35bfcda6a00b702
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-13 20:59:50 +0900

    Title: 갈 수 있는 곳들, Time: 62ms, Memory: 17MB, Status: Passed - CodeTree

--- 250313/갈 수 있는 곳들/places-can-go.py
+++ 250313/갈 수 있는 곳들/places-can-go.py
@@ -16,19 +16,22 @@ d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 res = 0
 
 visited = []
+q = deque([])
 
 for start in starts:
     si, sj = start[0] - 1, start[1] - 1
-    visited.append((si, sj))
-    q = deque([(si, sj)])
+    if (si, sj) not in visited:
+        visited.append((si, sj))
+        q.append((si, sj))
+        res += 1
+        while q:
+            ci, cj = q.popleft()
 
-    while q:
-        ci, cj = q.popleft()
+            for di, dj in d:
+                ni, nj = ci + di, cj + dj
+                if 0 <= ni < N and 0 <= nj < N and (ni, nj) not in visited and matrix[ni][nj] == 0:
+                    visited.append((ni, nj))
+                    q.append((ni, nj))
+                    res += 1
 
-        for di, dj in d:
-            ni, nj = ci + di, cj + dj
-            if 0 <= ni < N and 0 <= nj < N and (ni, nj) not in visited and matrix[ni][nj] == 0:
-                visited.append((ni, nj))
-                q.append((ni, nj))
-                res += 1
 print(res)
