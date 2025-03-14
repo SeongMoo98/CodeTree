46ad80562944893abae7095b12200dd84df7ffd2
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-14 15:17:14 +0900

    Title: 배열 추출, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250314/배열 추출/array-extraction.py
@@ -0,0 +1,18 @@
+# 1. x가 0이 아닌 자연수라면 x를 배열에 삽입
+# 2. x가 0이면 배열에서 가장 큰 값을 출력하고 그 값을 제거
+
+import heapq
+
+N = int(input())
+pq = []
+
+heapq.heapify(pq)
+
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if pq:
+            print(-heapq.heappop(pq))
+    else:
+        heapq.heappush(pq, -x)
+
