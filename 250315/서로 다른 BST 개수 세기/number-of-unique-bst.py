5bad6a07506c43a813c088f6bf35e03616d0fa0f
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-15 17:41:06 +0900

    Title: 서로 다른 BST 개수 세기, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250315/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 250315/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -2,21 +2,23 @@
 
 N = int(input())
 
+if N <= 1:
+    print(1)
+else:
+    dp = [0]*(N+1)
+    # 0 : 아무것도 없는 상태가 1로 취급
+    dp[0], dp[1], dp[2] = 1, 1, 2
 
-dp = [0]*(N+1)
-# 0 : 아무것도 없는 상태가 1로 취급
-dp[0], dp[1], dp[2] = 1, 1, 2
+    # ex) N = 4
+    # root 1 : 오른쪽에 2,3,4
+    # root 2 : 왼쪽에 1, 오른쪽에 3,4
+    # root 3 : 왼쪽에 1, 2, 오른쪽에 4
+    # root 4 : 오른쪽에 1,2,3
 
-# ex) N = 4
-# root 1 : 오른쪽에 2,3,4
-# root 2 : 왼쪽에 1, 오른쪽에 3,4
-# root 3 : 왼쪽에 1, 2, 오른쪽에 4
-# root 4 : 오른쪽에 1,2,3
+    # ==> dp[0]*dp[3], dp[1]*dp[2], dp[2]*dp[1], dp[3],dp[0]
 
-# ==> dp[0]*dp[3], dp[1]*dp[2], dp[2]*dp[1], dp[3],dp[0]
+    for i in range(3, N+1):
+        for j in range(i):
+            dp[i] += dp[j] * dp[i-j-1]
 
-for i in range(3, N+1):
-    for j in range(i):
-        dp[i] += dp[j] * dp[i-j-1]
-
-print(dp[N])
+    print(dp[N])
