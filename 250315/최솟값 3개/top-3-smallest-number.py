ba94c44ba62480b6820ed18fb98438547108a130
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-15 22:25:47 +0900

    Title: 최솟값 3개, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250315/최솟값 3개/top-3-smallest-number.py
@@ -0,0 +1,17 @@
+import heapq
+N = int(input())
+
+nums = list(map(int, input().split()))
+
+# N개의 수가 하나씩 주어질때, 지금까지 주어진 숫자들 중 가장 작은 3개의 곱을 출력
+# 주어진 수가 3개가 되지 않는다면 -1 출력
+
+for i in range(N):
+    if i < 3:
+        print(-1)
+    else:
+        res = 1
+        smallest_three = heapq.nsmallest(3, nums)
+        for j in range(3):
+            res *= smallest_three[j]
+        print(res)
