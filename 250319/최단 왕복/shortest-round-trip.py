a94af66f7fd6cc076738a5aaedda69cfa8359129
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-19 00:52:05 +0900

    Title: 최단 왕복, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250319/최단 왕복/shortest-round-trip.py
+++ 250319/최단 왕복/shortest-round-trip.py
@@ -1,21 +1,22 @@
-# 서로 크기가 다른 정수 N개에 대해
-# 1 ~ N번까지의 정수중 중 임의의 두 정수와 크기를 비교한 결과가 M번 주어진다
+# N개의 정점과 M개의 간선
+# 임의의 서로 다른 두 정점 사이를 왕복하는데 이용하는 
+# Edge들의 가중치 총합이 가장 낮은경우를 구하여라
 
-# M번의 비교결과를 통해, 각 정수가 다른 몇 개의 정수와 크기 비교 결과를 알수없는지 구하여라
+# Directed Graph
 
 N, M = map(int, input().split())
 
 INF = float('inf')
-graph = [[INF]*(N+1) for _ in range(N+1)]
-# 두 개의 정수가 공백을 두고 주어지는데. 앞의 정수가 뒤의 정수보다 숫자가 크다는 뜻
 
-for _ in range(M):
-    a, b = map(int, input().split())
-    # a > b라는 뜻
-    # 이를 1이라고 하자
-    graph[a][b] = 1
+graph = [[INF] * (N+1) for _ in range(N+1)]
+
+for i in range(M):
+    u, v, d = map(int, input().split())
+    graph[u][v] = d
+
 for i in range(1, N+1):
     graph[i][i] = 0
+
 
 def floyd_warshall(graph):
     for k in range(1, N+1):
@@ -23,11 +24,11 @@ def floyd_warshall(graph):
             for j in range(1, N+1):
                 if graph[i][j] > graph[i][k] + graph[k][j]:
                     graph[i][j] = graph[i][k] + graph[k][j]
-# graph[i][k], graph[k][j] ==> i > k > j
 
-floyd_warshall(graph0
-)
+floyd_warshall(graph)
+
+res = INF
 for i in range(1, N+1):
-    print(graph[i].count(INF))
-
-
+    for j in range(1, N+1):
+        res = min(res, graph[i][j] + graph[j][i])
+print(res)
