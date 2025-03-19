69a2519946e6c4bc1b1799e220a6af01088bb826
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-20 01:58:25 +0900

    Title: 최소 스패닝 트리 9, Time: 824ms, Memory: 54MB, Status: Passed - CodeTree

--- 250320/최소 스패닝 트리 9/minimum-spanning-tree-9.py
+++ 250320/최소 스패닝 트리 9/minimum-spanning-tree-9.py
@@ -15,26 +15,26 @@ def prim(graph, start):
     MST = []
     total_weight = 0
     pq = []
-    visited = []
+    visited = [False] * (N+1)
 
     # 출발 Vertex에 연결된 Vertex들
     for w, v in graph[start]:
         heapq.heappush(pq, (w, start, v))
     # 시작점을 방문 처리
-    visited.append(start)
+    visited[start] = True
 
     while pq:
         w, u, v = heapq.heappop(pq)
 
         # Weight가 가장 작고, Vertex v를 방문 x일 때
-        if v not in visited:
-            visited.append(v)
+        if visited[v] == False:
+            visited[v] = True
             MST.append((u, v, w))
             total_weight += w
 
             # Vertex v에 연결된 Vertex 확인
             for next_weight, next_node in graph[v]:
-                if next_node not in visited:
+                if visited[next_node] == False:
                     heapq.heappush(pq, (next_weight, v, next_node))
 
     return MST, total_weight
