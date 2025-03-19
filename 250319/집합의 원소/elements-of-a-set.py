d3339c2b413e8b4d22d7ff8a22436c969350717a
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-19 21:00:22 +0900

    Title: 집합의 원소, Time: 92ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250319/집합의 원소/elements-of-a-set.py
@@ -0,0 +1,36 @@
+# 집합 관리 프로그램
+# 초기 : 자기 자신만이 포함된 집합
+
+# 0 a b : a가 포함된 집합과 b과 포함된 집합을 합친다.
+# 1 a b : a와 b가 같은 집합에 있다면 1, 그렇지 않다면 0
+
+N, M = map(int, input().split())
+
+parent = [i for i in range(1, N+1)]
+
+def find(x):
+    if parent[x] == x:
+        return x
+
+    return find(parent[x])
+
+def union(a, b):
+    parent_a, parent_b = find(a), find(b)
+
+    if parent_a != parent_b:
+        parent[a] = b
+
+for _ in range(M):
+    num, a, b = map(int, input().split())
+    # Union
+    if num == 0:
+        union(a, b)
+    # Find
+    elif num == 1:
+        a, b = find(a), find(b)
+        if a == b:
+            print(1)
+        else:
+            print(0)
+            
+            
