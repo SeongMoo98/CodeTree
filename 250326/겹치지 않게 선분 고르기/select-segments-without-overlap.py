fe6dad0691dd5bc44bc7347b2872e386b7386290
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-26 12:46:32 +0900

    Title: 겹치지 않게 선분 고르기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250326/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 250326/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -3,14 +3,14 @@
 # 끝점을 겅유하는 것 역시 겹친 것
 
 N = int(input())
-lines = [list(map(int, input().split())) for _ in range(N)]
+lines = [[0, 0]] + [list(map(int, input().split())) for _ in range(N)]
 
 res = 0
 
 def backtrack(curr, ans, visited):
     global res
-    if curr == N:
-        res = max(res, len(ans))
+    if curr == N+1:
+        res = max(res, len(ans)-1)
         return 
 
     l, r = lines[curr]
@@ -20,10 +20,13 @@ def backtrack(curr, ans, visited):
     else:
         visited[l:r+1] = [True] * len(visited[l:r+1])
 
-    for i in range(curr, N):
+    for i in range(curr, N+1):
         ans.append(lines[i])
         backtrack(i+1, ans, visited)
         ans.pop() 
+
+# 이건 Line 0을 꼭 포함하는 코드다
+# backtrack(0, [], [False] * 1001)
 
 backtrack(0, [], [False] * 1001)
 print(res)
