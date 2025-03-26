64d0aa1b458f63fb7a5744ee93f6bf6110abff15
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-26 15:18:13 +0900

    Title: 십자 모양 폭발, Time: 70ms, Memory: 18MB, Status: Passed - CodeTree

--- 250326/십자 모양 폭발/cross-shape-bomb.py
+++ 250326/십자 모양 폭발/cross-shape-bomb.py
@@ -1,30 +1,57 @@
-# N개의 층으로 이루어진 1차원 젠가
+# N x N 격자판
 
-# 2번에 걸쳐 특정 구간의 블럭들을 뺴는 작업 
-# 남은 블럭은 중력에 의해 떨어져 남게된다.
+# 특정위치를 선택하면, 그 위치를 중심으로 십자모양으로 폭탄이 터짐
+# 터진 이후에는 중력에 의해 숫자들이 아래로 떨어짐
+
+# 십자 모양의 크기는 선택된 숫자에 비례하여 커짐
+# ex) 4 : 상하좌우 3칸
+
+# 폭탄이 터져야하는 범위가 격자 판을 벗어나도 격자안에서만 터짐
+# 터진 칸은 0
 
 N = int(input())
 
-blocks = [0] + [int(input()) for _ in range(N)]
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-for _ in range(2):
-    # start ~ end 블록 제거
-    # 나머지는 중력에 의해 아래로 이동
-    start, end = map(int, input().split())
-    temp = []
-    for i in range(1, len(blocks)):
-        if start <= i <= end:
-            continue
-        temp.append(blocks[i])
-    blocks = [0]
-    for num in temp:
-        blocks.append(num)
+# [r][c]에서 터지는데 해당 칸의 수에 맞는 폭탄 크기
+r, c = map(lambda x : int(x)-1, input().split())
+size = arr[r][c]
+
+bomb_idx = [(r, c)]
+for s in range(1, size):
+    if 0 <= r + s < N and 0 <= c < N:
+        bomb_idx.append((r+s, c))
+
+    if 0 <= r - s < N and 0 <= c< N:
+        bomb_idx.append((r-s, c))
+
+    if 0 <= r < N and 0 <= c + s < N:
+        bomb_idx.append((r, c+s))
     
+    if 0 <= r  < N and 0 <= c - s < N:
+        bomb_idx.append((r, c-s))
 
-if len(temp) == 0:
-    print(0)
-else:
-    print(len(temp))
-    for i in range(len(temp)):
-        print(temp[i])
+# 폭탄 처리
+for i in range(N):
+    for j in range(N):
+        if (i, j) in bomb_idx:
+            arr[i][j] = 0
+    
+temp_arr = [[0] * N for _ in range(N)]
 
+for j in range(N):
+    res = []
+    for i in range(N-1, -1, -1):
+        if arr[i][j] != 0:
+            res.append(arr[i][j])
+    idx = N-1
+    for i in range(len(res)):
+        temp_arr[idx-i][j] = res[i]
+
+for i in range(N):
+    for j in range(N):
+        print(temp_arr[i][j], end=' ')
+    print()
+
+
+    
