03dc4a4958a4d8b752bbf49bf7c5b975ff0fd8f4
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-03-27 21:33:23 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 73ms, Memory: 17MB, Status: Passed - Codetree

--- 250327/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 250327/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -5,17 +5,17 @@ INF = float('inf')
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-if N == 1:
-    print(arr[0][0])
-else:
-    dp =[[INF] * (N+1)] + [[INF] + [0] * N for _ in range(N)]
+dp = [[0] * N for _ in range(N)]
+dp[0][0] = arr[0][0]
 
-    dp[1][1] = arr[0][0]
+for i in range(1, N):
+    dp[i][0] = min(dp[i-1][0], arr[i][0])
+for j in range(1, N):
+    dp[0][j] = min(dp[0][j-1], arr[0][j])
 
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            dp[i][j] = min(dp[i-1][j], dp[i][j-1], arr[i-1][j-1])
-
-    print(max(dp[N-1][N], dp[N][N-1], dp[N-1][N-1]))
+for i in range(1, N):
+    for j in range(1, N):
+        dp[i][j] = max(min(dp[i-1][j], arr[i][j]), min(dp[i][j-1], arr[i][j]))
+print(dp[N-1][N-1])
 
 
