1464a7cc49286496d9afe3dcd2ab8de0992e496f
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2025-04-04 23:14:03 +0900

    Title: 청소는 즐거워, Time: 536ms, Memory: 24MB, Status: Passed - Codetree

--- 250404/청소는 즐거워/cleaning-is-joyful.py
+++ 250404/청소는 즐거워/cleaning-is-joyful.py
@@ -14,126 +14,82 @@
 
 N = int(input())
 
-# (i, j)에서 curr : (i, j-1)로 움직일 때
-# i, j 기준
-browse = [
-    ((-1, 0), 0.01),
-    ((1, 0), 0.01),    
-    ((-2, -1), 0.02),
-    ((-1, -1), 0.07),
-    ((1, -1), 0.07),
-    ((2, -1), 0.02),
-    ((-1, -2), 0.1),
-    ((1, -2), 0.1),
-    ((0, -3), 0.05),
-    ((0, -2), -1),
-]
-        
+matrix = [list(map(int, input().split())) for _ in range(N)]
 
+# 0 : 좌 -> 1 : 하 -> 2 : 우 -> 3: 상
+dust_ratio={
+    0:[ [0, 0, 0.02, 0, 0],
+        [0, 0.1, 0.07, 0.01, 0],
+        [0.05, 0, 0, 0, 0],
+        [0, 0.1, 0.07, 0.01, 0],
+        [0, 0, 0.02, 0, 0] ],
 
-dust = [list(map(int, input().split())) for _ in range(N)]
+    1:[ [0, 0, 0, 0, 0],
+        [0, 0.01, 0, 0.01, 0],
+        [0.02, 0.07, 0, 0.07, 0.02],
+        [0, 0.1, 0, 0.1, 0],
+        [0, 0, 0.05, 0, 0] ],
 
-res = 0
-# 빗자루가 왼쪽으로 움직일 때
-def get_dust(ci, cj):
-    global res
-    if cj == 0:
-        return
-    
-    curr_dust = dust[ci][cj-1]
-    # curr은 0
-    dust[ci][cj-1] = 0
-    dust_sum = 0
-    for (di, dj), r in browse:
-        ni, nj = ci + di, cj + dj
+    2:[ [0, 0, 0.02, 0, 0],
+        [0, 0.01, 0.07, 0.1, 0],
+        [0, 0, 0, 0, 0.05],
+        [0, 0.01, 0.07, 0.1, 0],
+        [0, 0, 0.02, 0, 0] ],
 
-        move_dust = int(curr_dust * r)
+    3:[ [0, 0, 0.05, 0, 0],
+        [0, 0.1, 0, 0.1, 0],
+        [0.02, 0.07, 0, 0.07, 0.02],
+        [0, 0.01, 0, 0.01, 0],
+        [0, 0, 0, 0, 0] ],
+}
 
-        # 범위 안 먼지
-        if 0 <= ni < N and 0 <= nj < N:
-            if r > 0:
-                dust_sum += move_dust
-                dust[ni][nj] += move_dust
-            # a % : 나머지
-            else:
-                move_dust = curr_dust - dust_sum
-                dust[ni][nj] += move_dust
-        # 범위 밖 먼지
-        else:
-            if r > 0:
-                dust_sum += move_dust
-                res += move_dust
-            else:
-                move_dust = curr_dust - dust_sum
-                res += move_dust
-            
+directions = {
+    0 : (0, -1),
+    1 : (1, 0),
+    2 : (0, 1),
+    3 : (-1, 0)
+}
 
-# 시계 방향 회전전
-def rotate():   
-    global dust
-    dust = list(map(list, zip(*dust[::-1])))
-
-# 나선형 회전
-# 좌 1, 하 1, 우 2, 상 2, 좌 3, 하 3, 우 4, 상 4, 마지막 좌 4한번 더
-# 즉, N-1번까지 회전
-
-
-# 좌, 하, 우, 상
-dirs = [0, 1, 2, 3]
-d = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-
-# (방향, 이동횟수)
-move = []
-idx = 0
-for step in range(1, N):
-    for j in range(2):
-        move.append((dirs[idx], step))
-        idx = (idx + 1) % 4
-move.append((0, N-1))
-        
-# 출발점
-si, sj = (N-1)//2, (N-1)//2
-ci, cj = si, sj
+ci, cj = N//2, N//2
 ei, ej = 0, 0
+res = 0
+def add_dust(ci, cj, dust):
+    global res
 
-# 방향, 이동횟수
-for move_dir, count in move:
-    for _ in range(count):
-        di, dj = d[move_dir]
-        
-        if move_dir == 0:
-            get_dust(ci, cj)
-        # 하 : 1번 회전
-        if move_dir == 1:
-            rotate()
-            ci, cj = cj, (N-1-ci)
+    if 0 <= ci < N and 0 <= cj < N:
+        matrix[ci][cj] += dust
+    else:
+        res += dust
+    
 
-            get_dust(ci, cj)
+def move():
+    global ci, cj
 
-            for _ in range(3):
-                rotate()
-                ci, cj = cj, (N-1-ci)
-        # 우 : 2번 회전전
-        if move_dir==2:
-            for _ in range(2):
-                rotate()
-                ci, cj = cj, (N-1-ci)
+    ci, cj = ci + directions[move_dir][0], cj + directions[move_dir][1]
 
-            get_dust(ci, cj)
+    dust_sum = 0
+    for i in range(5):
+        for j in range(5):
+            dust = int(matrix[ci][cj] * dust_ratio[move_dir][i][j])
+            add_dust(ci + i -2, cj + j -2 , dust)
 
-            for _ in range(2):
-                rotate()
-                ci, cj = cj, (N-1-ci)
-        # 상 :3번 회전
-        if move_dir == 3:
-            for _ in range(3):
-                rotate()
-                ci, cj = cj, (N-1-ci)
-                
-            get_dust(ci, cj)
+            dust_sum += dust
 
-            rotate()
-            ci, cj = cj, (N-1-ci)
-        
-        ci, cj = ci + di, cj + dj
+    add_dust(ci + directions[move_dir][0], cj + directions[move_dir][1],
+             matrix[ci][cj] - dust_sum)
+
+# 초기 방향, 초기 횟수
+move_dir, move_num = 0, 1
+while (ci, cj) != (ei, ej):
+    for _ in range(move_num):
+        move()
+
+        if (ci, cj) == (ei, ej):
+            break
+
+    move_dir = (move_dir + 1) % 4
+
+    if move_dir == 0 or move_dir == 2:
+        move_num += 1
+
 print(res)
